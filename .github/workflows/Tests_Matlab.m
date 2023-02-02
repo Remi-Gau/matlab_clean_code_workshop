0286e25c427c528cd748928593cab527df0dfa40
Remi Gau <remi_gau@hotmail.com>
2023-02-02 14:38:59 +0100

    adapt script to run locally

--- .github/workflows/Tests_Matlab.m
+++ .github/workflows/Tests_Matlab.m
@@ -1,11 +1,22 @@
 % (C) Copyright 2023 Remi Gau developers
 
+curent_directory = pwd;
+
 root_dir = getenv('GITHUB_WORKSPACE');
 
-addpath(fullfile(root_dir, 'MOcov', 'MOcov'));
+% set up for github CI
+if ~isempty(root_dir)
+    addpath(fullfile(root_dir, 'MOcov', 'MOcov'));
 
-cd(fullfile(root_dir, 'MOxUnit', 'MOxUnit'));
-run moxunit_set_path();
+    cd(fullfile(root_dir, 'MOxUnit', 'MOxUnit'));
+    run moxunit_set_path();
+
+    % set up for local
+else
+    root_dir = fullfile(fileparts(mfilename('fullpath')), '..', '..');
+end
 
 cd(fullfile(root_dir, 'part_1'));
-run run_tests;
+run Run_Tests;
+
+cd(curent_directory);
