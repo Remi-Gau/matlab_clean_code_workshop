48248579a95d96de7e5ea652273dce8237684075
Remi Gau <remi_gau@hotmail.com>
2023-02-02 18:03:35 +0100

    rm compiled results

---
+++ test/test_Analyse.m
@@ -0,0 +1,25 @@
+function test_suite = test_Analyse %#ok<STOUT>
+    % (C) Copyright 2023 Remi Gau developers
+    try % assignment of 'localfunctions' is necessary in Matlab >= 2016
+        test_functions = localfunctions(); %#ok<NASGU>
+    catch % no problem; early Matlab versions can use initTestSuite fine
+    end
+    initTestSuite;
+end
+
+function test_Analyse_basic()
+
+    root_dir = '/home/remi/github/matlab_clean_code_workshop/';
+    data_dir = fullfile(root_dir, 'data');
+    subject_dir = fullfile(data_dir, 'sub-01');
+    
+    cd(subject_dir)
+    Analyse()
+
+    old_data = load(fullfile(subject_dir, 'Behavioral', 'Results_PIEMSI_1.mat'));
+    new_data = load(fullfile(subject_dir, 'Behavioral', 'Results_PIEMSI_1_new.mat'));
+
+    assertEqual(old_data.NbValidTrials, new_data.NbValidTrials)
+    assertEqual(old_data.NbMcGURKinCON, new_data.NbMcGURKinCON)
+    
+end
